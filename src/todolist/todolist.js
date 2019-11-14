cb40c0efc7a02bab569be73a3aaba1415928d859
George Random <ninjavolant@hotmail.com>
2019-11-14 12:11:48 +0100

    plus beau

--- src/todolist/todolist.js
+++ src/todolist/todolist.js
@@ -73,7 +73,7 @@ function makeTodo({name,
         checked : false
     })
 }
-
+// gros container
 class TodoListApp extends React.Component {
     constructor(props){
         super(props);
@@ -108,37 +108,77 @@ class TodoListApp extends React.Component {
     } */
 
     handleProjectSelect = (num)=>{
-        let newcurr=parseInt(num,10)
+        let newcurr=parseInt(num,10) || null
         this.setState({current : newcurr})
     }
     render(){
         return (
          <div>   
-            <div className='App-header'>
-            <h1 span='2'>POUET</h1>
-            </div>
+            
             <div className='container'>
             
             <LeftPanel projects={this.state.projects}
             onProjectSelect={this.handleProjectSelect} 
             current={this.state.current}/>
             <RightPanel list={this.state.list} 
-            current={this.state.current}/>
+            current={this.state.current}
+            projects={this.state.projects}/>
             </div>
         </div>
         )
     }
 }
 
+//PANELS
+
 class LeftPanel extends React.Component {
     
     render(){
         return(
             <div className='left-container'>
-                <ProjectList projects={this.props.projects}
-                selectProject={this.props.onProjectSelect} 
-                current={this.props.current}
-                />
+                <div className='left-header'>
+                    <LeftHeader />
+
+                </div>
+                <div className='left-display'>
+                    <ProjectList projects={this.props.projects}
+                    selectProject={this.props.onProjectSelect} 
+                    current={this.props.current}
+                    />
+                    </div>
+                <div className = 'left-ctrls'>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+class RightPanel extends React.Component {
+
+    render(){
+        return (
+        <div className='right-container'>
+            <div className='right-display'> 
+            <RightTitle 
+            current={this.props.current} 
+            projects={this.props.projects}
+            />
+            <TodoDisplay list={this.props.list} 
+            current={this.props.current}
+            /> </div>
+            <div className='right-controls'> <RightCtrls /> </div> 
+        </div>
+        )
+    }
+}
+
+//INSIDE LEFT PANEL
+class LeftHeader extends React.Component {
+    render(){
+        return (
+            <div className='left-header'>
+            <h1 span='2'>POUET</h1>
             </div>
         )
     }
@@ -148,11 +188,19 @@ class ProjectList extends React.Component {
     
     clickProject = (event) =>{
         let selected = event.target.dataset.id;
+        console.log(parseInt(selected,10))
         this.props.selectProject(selected)
     }
     
     render(){
         let list=this.props.projects;
+        let allbutton = <li>
+            <button className='select-all-projects'
+            onClick={this.clickProject}
+            data-id= {'null'} >
+                SELECT ALL
+            </button>
+        </li>
         if (list.length===0){
             return (
                 <h1>
@@ -162,39 +210,33 @@ class ProjectList extends React.Component {
         }
         else
 
-        return list.map((project)=>{
-            return (
-                <li key={project.number}>
-                {project.number} | {project.title} |
-                <button className='select-project'
-                 onClick={this.clickProject} 
-                 data-id = {project.number}
-                 > select</button>
-                </li>
-                
-                
-            )
-        })
+        return (
+            <div id='project-list'>
+                {
+                list.map((project)=>{
+                return (
+                    <li key={project.number}>
+                    {project.number} | {project.title} |
+                    <button className='select-project'
+                    onClick={this.clickProject} 
+                    data-id = {project.number}
+                    > select</button>
+                    </li>
+                    )
+                })
+                }
+                {allbutton}
+            </div>
+        )
+        
+        
     }
 
         
 
     
 }
-class RightPanel extends React.Component {
-
-    render(){
-        return (
-        <div className='right-container'>
-            <div className='todo-display'> 
-            <TodoDisplay list={this.props.list} 
-            current={this.props.current}
-            /> </div>
-            <div className='right-controls'> <RightCtrls /> </div> 
-        </div>
-        )
-    }
-}
+//INSIDE RIGHT PANEL
 class TodoDisplay extends React.Component {
     sortByProj = (list,num)=>{
         if (num === null){return list} else
@@ -220,6 +262,27 @@ class TodoDisplay extends React.Component {
     } 
 
 }
+class RightTitle extends React.Component {
+    render(){
+        let p=this.props.current;
+        let titletext = 'FULL LIST';
+        let subtitle = 'all the todos'
+        if (p) {
+            let currentp = this.props.projects.find((proj)=>{
+                return (proj.number === p)
+                })
+
+            titletext = currentp.title
+            subtitle = currentp.desc
+        }
+        return(
+            <div className = 'todo-title'>
+            <h1>{titletext}</h1>
+            <h3>{subtitle}</h3>
+            </div>
+        )
+    }
+}
 class RightCtrls extends React.Component {
     render(){
         return(
@@ -228,7 +291,9 @@ class RightCtrls extends React.Component {
     }
 }
 
-class NewTodoForm extends React.Component {
+
+
+/* class NewTodoForm extends React.Component {
     constructor(props){
         super(props);
         this.state = {
@@ -265,7 +330,7 @@ class NewTodoForm extends React.Component {
             </form>
         )
     }
-}
+} */
 
 
 
